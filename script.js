b0300b1f8b3d99b3f4acd9f010620ca3d65326dc
nickr <nicholaspriegel@gmail.com>
2024-03-27 13:52:51 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,54 @@
+/*
+Exercise 1: maxOfTwoNumbers
+
+In this exercise, create a function named maxOfTwoNumbers. It should take two numbers as inputs and return the larger number. If they're equal, return either one.
+
+Exercise 1 has been completed for you:
+*/
+
+// const maxOfTwoNumbers = (x, y) => {
+//   if (x >= y) {
+//     return x;
+//   } else {
+//     return y;
+//   }
+// }
+
+// console.log('Exercise 1 Result:', maxOfTwoNumbers(4, 3));
+
+/*
+Exercise 2: maxOfThree
+
+Define a function, maxOfThree, it should accept three numbers and return the largest among them.
+
+Complete Exercise 2 in the space below:
+*/
+
+// const maxOfThreeNumbers = (x, y, z) => {
+//   if (x >= y && x >= z) {
+//     return x;
+//   } else if (y >= x && y >=z){
+//     return y;
+//   } else {
+//     return z;
+//   }
+// }
+
+// console.log('Exercise 2 Result:', maxOfThreeNumbers(8, 7, 9));
+
+/*
+Exercise 3: Insert at the Beginning
+
+Write a function named isCharAVowel that takes a single character as an argument. It should return true if the character is a vowel, and false otherwise.
+
+Complete Exercise 3 in the space below:
+*/
+
+const isCharAVowel = (char) => {
+  if (char === 'a' || char === 'e' || char === 'i' || char === 'o' || char === 'u'){
+    return true;
+  } else {
+    return false;
+  }
+}
+console.log('Exercise 3 Result:', );
